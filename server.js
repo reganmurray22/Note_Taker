a550650d3055fbb1d2614aa91cf2dbdb06ef09de
reganmurray22 <67241915+reganmurray22@users.noreply.github.com>
2020-11-25 23:48:58 -0600

    html and api routes

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+require("./routes/htmlroute")(app);
+require("./routes/apiroute")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
